63d70a8183f9c2a04754dc0735be0a78681c57a4
fakeLemonz <tshabalalatity14@gmail.com>
2024-12-23 12:20:59 +0200

    Update variable

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,12 +1,12 @@
 var_task = input("Enter your task: ")
-var_priority = input("Priority (high/medium/low): ")
+priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
 
-match var_priority:
+match priority:
     case "high" if time_bound =="yes":
-        print(f"'{var_task}' is a {var_priority} task that requires immediate attention today.")
+        print(f"'{var_task}' is a {priority} task that requires immediate attention today.")
     case "medium" | "low" if time_bound =="no":
-        print(f"Note: '{var_task}' is a {var_priority} task. Consider completing it when you have free time.")
+        print(f"Note: '{var_task}' is a {priority} task. Consider completing it when you have free time.")
     case _:
         print("Invalid input.")
