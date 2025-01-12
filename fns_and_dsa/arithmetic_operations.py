6eb766f32e8b6bbcb9f636868f47778e0fed56c7
fakeLemonz <tshabalalatity14@gmail.com>
2025-01-12 16:49:06 +0200

    Update arithmetic module

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,11 +4,11 @@ def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
-        return num2 - num1
+        return num1 - num2
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num1 == 0:
+        if num2 == 0:
             return ZeroDivisionError("Cannot divide by 0.")
-        return num2 // num1
+        return num1 // num2
     
