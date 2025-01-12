e2cdbe1f2d5022e273911310d0ed94590f35467e
fakeLemonz <tshabalalatity14@gmail.com>
2025-01-12 23:06:59 +0200

    Add number_of_days variable

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,11 +2,12 @@ import datetime
 # Function to display current date
 def display_current_date():
     current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Current date and time: {current_date}")
+    print(f"Current date and time: {current_date}") #Print out current datetime
 
 def calculate_future_date():
+    number_of_days = int(input("Enter the number of days: "))
     current_date_time = datetime.datetime.now()
-    days = datetime.timedelta(days=int(input("Enter the number of days: ")))
+    days = datetime.timedelta(days=number_of_days)
 
     future_date = current_date_time + days
     future_date = future_date.strftime("%Y-%M-%D %H:%M:%S")
