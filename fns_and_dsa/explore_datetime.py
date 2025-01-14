ceb98d629e1c510cbe4c0e412853903c100c80c1
fakeLemonz <tshabalalatity14@gmail.com>
2025-01-14 23:03:40 +0200

    Update variable name to display_current_datetime()

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,6 +1,6 @@
 from datetime import datetime 
 # Function to display current date
-def display_current_date():
+def display_current_datetime():
     current_date = datetime.datetime.now()
     current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {current_date}") #Print out current datetime
@@ -14,14 +14,14 @@ def calculate_future_date():
     future_date = future_date.strftime("%Y-%M-%D %H:%M:%S")
     print(f"Future date: {future_date}")
 
-display_current_date()
+display_current_datetime()
 calculate_future_date()
 
 
 
 def main():
     while True:
-        display_current_date()
+        display_current_datetime()
         calculate_future_date()
 
 
