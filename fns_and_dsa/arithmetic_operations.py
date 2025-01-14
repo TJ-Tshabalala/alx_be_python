a39471270ccded0e42472462cee098d9dbcbaaaf
fakeLemonz <tshabalalatity14@gmail.com>
2025-01-14 23:08:43 +0200

    Remove extra spaces in calculations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,15 +2,15 @@
 
 def perform_operation(num1,num2,operation):
     if operation == "add":
-        return num1 + num2
+        return num1+num2
     elif operation == "subtract":
-        return num1 - num2
+        return num1-num2
     elif operation == "multiply":
-        return num1 * num2
+        return num1*num2
     elif operation == "divide":
         if num2 == 0:
             return("Cannot divide by 0.")
-        return num1 // num2
+        return num1//num2
     
     def main():
         perform_operation()
