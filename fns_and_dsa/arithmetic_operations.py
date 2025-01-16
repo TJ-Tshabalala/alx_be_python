3a2de46a6d19403f474b3cb82b82c6c38687fbe6
fakeLemonz <tshabalalatity14@gmail.com>
2025-01-16 22:12:06 +0200

    Update data type in arithmetic_operations module

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,11 +10,11 @@ def perform_operation(num1,num2,operation):
     elif operation == "divide":
         if num2 == 0:
             return("Cannot divide by 0.")
-        return num1//num2
+        return num1/num2
     
 def main():
-        num1 = float(int(input("Enter the first number: ")))
-        num2 = float(int(input("Enter the second number: ")))
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
         operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
         result = perform_operation(num1, num2, operation)
         print(f"Result: {result}")
