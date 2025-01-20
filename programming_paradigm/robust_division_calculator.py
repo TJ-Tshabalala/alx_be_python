22ded503912855998ac7771cf9a5ce4b8d1e9574
fakeLemonz <tshabalalatity14@gmail.com>
2025-01-20 21:40:14 +0200

    Update robust division calculator module

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,6 @@
 def  safe_divide(numerator,denominator):
                 try:
-                    result =  float(numerator)//float(denominator)
+                    result =  float(numerator)/float(denominator)
                 except ValueError as e:
                    print("Error: Please enter numeric values only.")
                 except ZeroDivisionError as e:
@@ -8,5 +8,5 @@ def  safe_divide(numerator,denominator):
                 except Exception as e:
                    print(e)
                 else:
-                   return f"The result of the division is {result :.2f}"
+                   return f"The result of the division is {result}"
 
