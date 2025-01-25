fd0d7907a1f0ca0608794d7cfa8d535138ae1102
fakeLemonz <tshabalalatity14@gmail.com>
2025-01-25 23:12:34 +0200

    Add polymorphism module

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,20 @@
+import math
+class Shape:
+    def __init__(self, length=0, width=0):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return "Result of area for generic shape"
+    
+class Rectangle(Shape):
+
+    def area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
