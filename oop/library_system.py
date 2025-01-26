1c3a6db95b98c859fd8a986395e39d45e0ac894a
fakeLemonz <tshabalalatity14@gmail.com>
2025-01-26 19:51:00 +0200

    Update library module

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,33 +1,40 @@
 class Book:
-    def __init__(self, title:str , author: str):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
 class EBook(Book):
-    def __init__(self, title,author,file_size: int):
+    def __init__(self, title, author, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
-    
 
+    def __str__(self):
+        return f"{self.title} by {self.author}, File Size: {self.file_size}KB"
+    
 class PrintBook(Book):
-    def __init__(self, title, author, page_count: int):
+    def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
+    
 
 class Library:
     def __init__(self):
         self.books = []
-    
-    def add_book(self, book):
-        self.books = self.books.append[book]
 
-    def list_book(self):
-        books_list = []
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
         for book in self.books:
-            if(book, Book):
-                print(f"Book: {self.title} by {self.author}")
-            elif(book, EBook):
-                print(f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB")
-            elif(book, PrintBook):
-                print(f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}")
-    
+            if isinstance(book, Book):
+                print(f"Book: {book.title} by {book.author}")
+            elif isinstance(book, EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
+            elif isinstance(book, PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
